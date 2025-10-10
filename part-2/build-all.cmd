6f38154db79b6002d495e8cd4a3ccf1b0b08f9a4
Bert Laverman <bert.laverman@rakis.nl>
2025-10-10 16:22:13 +0200

    One more `build-all.cmd`

---
+++ part-2/build-all.cmd
@@ -0,0 +1,43 @@
+cd "2-1 Polling for Messages in C"
+call ..\..\build.cmd MessagePolling
+cd ..
+
+cd "2-2 Polling for Messages in C++"
+call ..\..\build.cmd MessagePolling
+cd ..
+
+cd "2-3 Windows Messaging in C"
+call ..\..\build.cmd WindowsMessaging
+cd ..
+
+cd "2-4 Windows Messaging in C++"
+call ..\..\build.cmd WindowsMessaging
+cd ..
+
+cd "2-5 Windows Event in C"
+call ..\..\build.cmd WindowsEvent
+cd ..
+
+cd "2-6 Windows Event in C++"
+call ..\..\build.cmd WindowsEvent
+cd ..
+
+cd "2-7 RequestSystemState in C"
+call ..\..\build.cmd RequestSystemState
+cd ..
+
+cd "2-8 RequestSystemState in C++"
+call ..\..\build.cmd RequestSystemState
+cd ..
+
+cd "2-9 Exception Messages and SendID in C++"
+call ..\..\build.cmd ExceptionMessages
+cd ..
+
+cd "2-10 SubscribeToSystemEvent in C"
+call ..\..\build.cmd SubscribeToSystemEvent
+cd ..
+
+cd "2-11 SubscribeToSystemEvent in C++"
+call ..\..\build.cmd SubscribeToSystemEvent
+cd ..
